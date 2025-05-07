bf320cdc17abc9a55547cdd6ba1c1f2d97459db5
Senthoor <skrishn3@terpmail.umd.edu>
2025-05-07 11:50:02 -0400

    added dollars per compute graph

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const margin = { top: 50, right: 200, bottom: 100, left: 80 };
-const width = 850 - margin.left - margin.right;
-const height = 600 - margin.top - margin.bottom;
+const width = 800 - margin.left - margin.right;
+const height = 700 - margin.top - margin.bottom;
 const tooltip = d3.select("#tooltip");
 
 
+let computeData = []
 let modelSizeData = []
 let benchmarkData = []
 let benchmarkTasks = ["Handwriting recognition", "Image recognition", "Language understanding", "Predictive reasoning", "Reading comprehension"]
@@ -11,12 +12,13 @@ let modelTypes = ["Biology", "Games", "Image generation", "Language", "Multiple 
 let modelColors = modelTypes.reduce((a, d, i) => { return { ...a, [d]: d3.schemeCategory10[i] } }, {})
 let benchmarkColors = benchmarkTasks.reduce((a, d, i) => { return { ...a, [d]: d3.schemeCategory10[i] } }, {})
 
-let timelineEndYear = 1950
+let timelineYear = 1950
+const computeStartYear = 1959
 const modelSizeStartYear = 1950
 const benchmarkStartYear = 1997
 
-let timeline = new TL.Timeline('timeline-embed', 'timeline.json');
 
+let timeline = new TL.Timeline('timeline-embed', 'timeline.json');
 
 timeline.on("change", (d) => {
     const data = timeline.getDataById(d?.unique_id)
@@ -29,15 +31,15 @@ timeline.on("change", (d) => {
 
 /* 
     TODO
-    - add  titles to graphs/short descriptors
     - add Computer per dollar graph
     - allow hovering/filtering points on params graph 
 */
 
 function updateGraphs(endYear) {
-    timelineEndYear = endYear
+    timelineYear = endYear
     UpdateModelSizeVis()
     UpdateBenchmarkVis()
+    UpdateComputeVis()
 }
 
 function addTitle(svg, title) {
@@ -53,13 +55,13 @@ function addTitle(svg, title) {
 
 
 function UpdateModelSizeVis() {
-    let data = modelSizeData.filter(d => { return (d.date.getYear() + 1900) <= timelineEndYear })
+    let data = modelSizeData.filter(d => { return (d.date.getYear() + 1900) <= timelineYear })
     const g = d3.select("#vis1").select("svg").select("g")
     const xAxis = g.select(".xAxis")
     const yAxis = g.select(".yAxis")
 
     const x = d3.scaleTime()
-        .domain([new Date(modelSizeStartYear, 0, 1), new Date(timelineEndYear, 0, 1)])
+        .domain([new Date(modelSizeStartYear, 0, 1), new Date(timelineYear, 0, 1)])
         .range([0, width]);
 
     const y = d3.scaleLog()
@@ -67,7 +69,7 @@ function UpdateModelSizeVis() {
         .range([height, 0]);
 
     const xAxisUpdated = d3.axisBottom(x).tickFormat(d => d3.timeFormat("%Y")(d));
-    const yAxisUpdated = d3.axisLeft(y);
+    const yAxisUpdated = d3.axisLeft(y).tickFormat(d3.format(".1e"));
 
     xAxis.transition()
         .duration(500)
@@ -107,7 +109,7 @@ function UpdateModelSizeVis() {
             return update.transition(500).attr("cy", d => y(d.paramSize)).attr("cx", d => x(d.date))
         },
         function (exit) {
-            return exit.transition().duration(500).attr("r", 0).remove()
+            return exit.transition().duration(300).attr("r", 0).remove()
         }
     )
 
@@ -120,7 +122,7 @@ function InitModelSizeVis(data) {
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
 
-    
+
     addTitle(svg, "AI Training Parameter Size Over Time");
     const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
@@ -203,17 +205,17 @@ function InitBenchmarkVis() {
         .attr('fill', 'none');
 
     svg.append("text")
-        .attr("transform", `translate(${margin.left + width / 3},${height / 4 + margin.top / 5})`)
+        .attr("transform", `translate(${margin.left + width / 3.3},${height / 4 + margin.top / 5})`)
         .style("text-anchor", "middle")
-        .text("Human Performance as benchmark (set to zero)")
+        .text("Human Benchmark Performance (set to zero)")
         .attr("opacity", .5)
-
+        .style("font-size", 14)
 
     UpdateBenchmarkVis()
 }
 
 function UpdateBenchmarkVis() {
-    let benchmarkEndYear = (timelineEndYear < benchmarkStartYear ? benchmarkStartYear : timelineEndYear)
+    let benchmarkEndYear = (timelineYear < benchmarkStartYear ? benchmarkStartYear : timelineYear)
     let data = benchmarkData.filter(d => { return (d.date.getYear() + 1900) <= benchmarkEndYear })
 
 
@@ -265,9 +267,13 @@ function UpdateBenchmarkVis() {
         },
         function (update) {
             return update
-            .transition().duration(500).attr("cy", d => y(d.score))
+                .transition().duration(500).attr("cy", d => y(d.score))
                 .attr("cx", d => x(d.date))
         },
+
+        function (exit) {
+            return exit.remove()
+        }
 
     )
 
@@ -285,6 +291,119 @@ function UpdateBenchmarkVis() {
     )
 
 }
+
+function UpdateComputeVis() {
+    let computeEndYear = (timelineYear < computeStartYear ? computeStartYear + 5 : timelineYear)
+    let data = computeData.filter(d => { return (d.date.getYear() + 1900) <= computeEndYear })
+
+    const g = d3.select("#vis3").select("svg").select("g")
+
+    const xAxis = g.select(".xAxis")
+    const yAxis = g.select(".yAxis")
+
+    const x = d3.scaleTime()
+        .domain([new Date(computeStartYear, 0, 1), new Date(computeEndYear, 0, 1)])
+        .range([0, width]);
+
+    const y = d3.scaleLog()
+        .domain(d3.extent(computeData, d => d.cost))
+        .range([height, 0]);
+
+    const yAxisUpdated = d3.axisLeft(y).tickFormat(d3.format(".1e"));
+    const xAxisUpdated = d3.axisBottom(x).tickFormat(d3.timeFormat("%Y"));
+
+    yAxis.transition()
+        .call(yAxisUpdated)
+
+
+    xAxis.transition()
+        .call(xAxisUpdated)
+
+
+    g.selectAll("circle").data(data).join(
+        function (enter) {
+            return enter.append("circle")
+
+                .on("mouseover", function (event, d) {
+                    if (d) {
+                        d3.select(this).style("cursor", "pointer")
+                        d3.select(this).attr("r", 12)
+                        tooltip.transition().duration(100).style("opacity", .95);
+                        tooltip.html(`<strong>${d.name}</strong>$${d.cost > 100000 ? d3.format(".2e")(d.cost) : d3.format(".3")(d.cost)}`)
+                            .style("left", (event.pageX + 10) + "px")
+                            .style("top", (event.pageY - 28) + "px");
+                    }
+                })
+                .on("mouseout", function () {
+                    d3.select(this).attr("r", 10)
+                    tooltip.transition().duration(300).style("opacity", 0);
+                })
+                .attr("r", 10)
+                .attr("fill", "#69b3a2")
+                .attr("stroke", "black")
+                .attr("opacity",.75)
+                .transition()
+                .duration(300)
+                .attr("cx", d => x(d.date))
+                .attr("cy", d => y(d.cost))
+
+                
+        },
+        function (update) {
+            return update
+                .transition().duration(500)
+                .attr("cy", d => y(d.cost))
+                .attr("cx", d => x(d.date))
+        },
+        function (exit) {
+            return exit.transition().duration(300).attr("r", 0).remove()
+        }
+    )
+}
+
+
+function InitComputeVis() {
+    const svg = d3.select("#vis3").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+    addTitle(svg, "U.S. Dollars Per Compute Over Time")
+
+    // Axis labels
+    svg.append("text")
+        .attr("transform", `translate(${margin.left + width / 2},${margin.top + height + 40})`)
+        .style("text-anchor", "middle")
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", margin.left - 60)
+        .attr("x", 0 - (margin.top + height / 2))
+        .style("text-anchor", "middle")
+        .text("Dollars Per GFLOP");
+
+    g.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .attr("class", "xAxis")
+
+
+    g.append("g").attr("class", "yAxis")
+    g.append("g").attr("class", "gLines")
+
+    UpdateComputeVis()
+}
+
+d3.csv("data2/dollars_per_compute.csv").then(data => {
+    data.forEach(d => {
+        d.cost = +d.GFLOPCost;
+        d.date = new Date(d.Day)
+        d.name = d.Entity
+    });
+
+    data.sort((a, b) => { return a.date - b.date })
+    computeData = data
+    InitComputeVis()
+})
 
 d3.csv("data2/ai_benchmark_progress.csv").then(data => {
     const parsedData = []
